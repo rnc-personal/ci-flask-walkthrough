9df5ec7c5627794ed408f118af46617b7fdf17f9
Richard Challenger <108702296+rnc-personal@users.noreply.github.com>
2023-01-28 13:48:46 +0000

    Init Commit

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+# Import  the flask class from the flask module
+
+app = Flask(__name__)
+# Defining an instance of the flask class with the name of the applications module as the parameter
+# (__name is a built in variable that returns the name of the module python module)
+
+
+@app.route('/')
+# decorator is wrapping the function and adding functionality to it
+# when someone hits the / endpoint, the function index() will be called
+def index():
+    return  "Hello World"
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv('IP', '0.0.0.0.'),
+        port = int(os.environ.get('PORT', 5000)),
+        debug = True)
